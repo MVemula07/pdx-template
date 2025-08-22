1f511a5806814f9a876d703b6214a0666dd2469c
MVemula07 <73973891+MVemula07@users.noreply.github.com>
2025-08-22 14:53:30 -0500

    Update tabmrg.js

--- content/assets/js/tabmrg.js
+++ content/assets/js/tabmrg.js
@@ -1,1 +1,20 @@
+try {
+  var currentTabIndex = sessionStorage.getItem('fhir-resource-tab-index');
+} catch(exception) {
+}
 
+if (!currentTabIndex)
+  currentTabIndex = '0';
+
+$('#tabs').tabs({
+  active: parseInt(currentTabIndex),
+  activate: function(event, ui) {
+    var active = ui.newTab.index();
+    currentTabIndex = active;
+    document.activeElement.blur();
+    try {
+      sessionStorage.setItem('fhir-resource-tab-index', currentTabIndex);
+    } catch(exception) {
+    }
+  }
+});
